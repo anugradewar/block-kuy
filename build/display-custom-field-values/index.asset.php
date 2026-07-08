c70c7caee56e12957a471c60d34f3f473e480d1e
Anugra Dewa Ramadhan <dewaramadhan20@gmail.com>
2026-07-08 10:26:02 +0700

    remove display custom fields block

--- build/display-custom-field-values/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '28046e31af33d02fe8a4');
